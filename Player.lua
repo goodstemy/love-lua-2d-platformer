8cad61b3b535c0c3493e729566916c193ed7290c
Ivan <ia_efremov@bingo-boom.com>
2019-06-29 10:05:31 +0300

    WIP on physics

--- Player.lua
+++ Player.lua
@@ -18,71 +18,25 @@ local horizontalFramesPaddings = {15, 110, 207, 304, 400, 495, 590, 685}
 
 function Player:new()
 	self.image = love.graphics.newImage('assets/Player/player.png')
-	self.x = 100
-	self.y = 100
-	self.speed = 100
+	-- self.x = 100
+	-- self.y = 100
+	-- self.speed = 100
 	loadFrames(self.image)
 end
 
 function Player:draw(x, y)
-	self.x = x
-	self.y = y
-	love.graphics.draw(self.image, activeFrame, self.x, self.y, 0, 1.5, 1.5)
+	love.graphics.draw(self.image, activeFrame, x, y, 0, 1.5, 1.5)
 end
 
 function Player:move(dt)
-	if love.keyboard.isDown('up') and love.keyboard.isDown('left') then
-		player.y = player.y - player.speed * dt
-		player.x = player.x - player.speed * dt
-		direction = 'left'
-		lastDirection = direction
-		currentTotalFrames = totalFrames.leftMovementTotalFrames
-	elseif love.keyboard.isDown('up') and love.keyboard.isDown('right') then
-		player.y = player.y - player.speed * dt
-		player.x = player.x + player.speed * dt
-		direction = 'right'
-		lastDirection = direction
-		currentTotalFrames = totalFrames.rightMovementTotalFrames
-	elseif love.keyboard.isDown('down') and love.keyboard.isDown('left') then
-		player.y = player.y + player.speed * dt
-		player.x = player.x - player.speed * dt
-		direction = 'left'
-		lastDirection = direction
-		currentTotalFrames = totalFrames.leftMovementTotalFrames
-	elseif love.keyboard.isDown('down') and love.keyboard.isDown('right') then
-		player.y = player.y + player.speed * dt
-		player.x = player.x + player.speed * dt
-		direction = 'right'
-		lastDirection = direction
-		currentTotalFrames = totalFrames.rightMovementTotalFrames
-	elseif love.keyboard.isDown('left') then
-		player.x = player.x - player.speed * dt
+	if love.keyboard.isDown('left') then
 		direction = 'left'
 		lastDirection = direction
 		currentTotalFrames = totalFrames.leftMovementTotalFrames
 	elseif love.keyboard.isDown('right') then
-		player.x = player.x + player.speed * dt
 		direction = 'right'
 		lastDirection = direction
 		currentTotalFrames = totalFrames.rightMovementTotalFrames
-	elseif love.keyboard.isDown('up') then
-		player.y = player.y - player.speed * dt
-		if lastDirection == 'left' then
-			direction = 'left'
-			currentTotalFrames = totalFrames.leftMovementTotalFrames
-		elseif lastDirection == 'right' then
-			direction = 'right'
-			currentTotalFrames = totalFrames.rightMovementTotalFrames
-		end
-	elseif love.keyboard.isDown('down') then
-		player.y = player.y + player.speed * dt
-		if lastDirection == 'left' then
-			direction = 'left'
-			currentTotalFrames = totalFrames.leftMovementTotalFrames
-		elseif lastDirection == 'right' then
-			direction = 'right'
-			currentTotalFrames = totalFrames.rightMovementTotalFrames
-		end
 	else
 		direction = nil
 		currentTotalFrames = totalFrames.standLeftTotalFrames
@@ -104,10 +58,6 @@ function move(dt)
 			activeFrame = leftMovementFrames[currentFrame]
 		elseif direction == 'right' then
 			activeFrame = rightMovementFrames[currentFrame]
-		elseif direction == 'up' then
-			activeFrame = upMovementFrames[currentFrame]
-		elseif direction == 'down' then
-			activeFrame = downMovementFrames[currentFrame]
 		else
 			if lastDirection == 'left' then
 				activeFrame = standLeftFrames[currentFrame]
